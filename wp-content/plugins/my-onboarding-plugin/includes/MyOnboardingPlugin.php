2d740dc48710be39dc8243e11222917c47f63098
lgadzhev-devrix <lgadzhev@devrix.com>
2018-10-31 13:00:24 +0200

    Day 6 task from the onboarding
    Created submenu in the Settings menu

--- wp-content/plugins/my-onboarding-plugin/includes/MyOnboardingPlugin.php
+++ wp-content/plugins/my-onboarding-plugin/includes/MyOnboardingPlugin.php
@@ -5,6 +5,7 @@ class MyOnboardingPlugin
     function __construct()
     {
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+        add_action('admin_menu', array($this, 'my_onboarding_submenu'));
     }
 
     /**
@@ -12,7 +13,6 @@ class MyOnboardingPlugin
      */
     function activate()
     {
-        $this->enqueue();
         flush_rewrite_rules();
     }
     /**
@@ -26,5 +26,20 @@ class MyOnboardingPlugin
     function enqueue()
     {
         wp_enqueue_script( 'mypluginscript', plugins_url( '../assets/script.js', __FILE__ ), array( 'jquery' ) );
+    }
+
+    function my_onboarding_submenu()
+    {
+        add_submenu_page('options-general.php',
+            'My Onboarding',
+            'My Onboarding',
+            'manage_options',
+            'my_onboarding',
+            'my_onboarding_page');
+    }
+
+    function my_onboarding_page()
+    {
+
     }
 }

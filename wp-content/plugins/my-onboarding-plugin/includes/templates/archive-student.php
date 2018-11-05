80319d44132b51796f0710bc124dd687bb6f46a0
lgadzhev-devrix <lgadzhev@devrix.com>
2018-11-05 12:12:56 +0200

    Onboarding: Day 9 tasks
    I added pagination to my custom archive page
    
    DevriX Dev Onboarding Tasks and document

--- wp-content/plugins/my-onboarding-plugin/includes/templates/archive-student.php
+++ wp-content/plugins/my-onboarding-plugin/includes/templates/archive-student.php
@@ -7,6 +7,7 @@ get_header();
 $limitToFour = new WP_Query( array(
 	'post_type'      => 'student',
 	'posts_per_page' => 4,
+	'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1
 ) );
 
 if ( $limitToFour->have_posts() ) : ?>
@@ -21,6 +22,29 @@ if ( $limitToFour->have_posts() ) : ?>
 		 */
 		get_template_part( 'template-parts/post/content', get_post_format() );
 	endwhile;
+
+	?>
+	<div class="pagination">
+    <?php
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $limitToFour->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
+    ?>
+</div>
+<?php
+
 	wp_reset_postdata();
 else :
 
